30a9beb8705d793f86feaae065a75aea40d23734
Mitchell Cohen <122713703+profmitchell@users.noreply.github.com>
2026-08-13 23:27:04 -0400

    Wire equipment probe into existing startup

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ clang++ \
   "${SRC}/axiom_runtime.cpp" \
   "${SRC}/axiom_force_service.mm" \
   "${SRC}/axiom_force_runtime.cpp" \
-  "${SRC}/capture_research.mm"
+  "${SRC}/capture_research_with_equipment.mm"
 
 codesign --force --sign - --timestamp=none "${OUT}"
 xattr -cr "${OUT}" 2>/dev/null || true
